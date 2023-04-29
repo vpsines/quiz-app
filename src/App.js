d3d7b10929944b76ff43916528c5364c351b0ee5
vpsines <sasip6028@gmail.com>
2023-04-29 22:23:23 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	// state variable for question index
+	const [currentIndex, setCurrentIndex] = useState(0)
+
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	]
+
+	function handleAnswerClick(isCorrect) {
+		//  if correct answer is pressed, increment score
+        if(isCorrect){
+            setScore((oldScore)=>oldScore+1)
+        }
+
+		if (currentIndex === questions.length - 1) {
+			// quiz over
+			setQuizFinished(true)
+		} else {
+			setCurrentIndex((value) => value + 1)
+		}
+	}
+
+  // state variable for quiz status
+	const [quizFinished, setQuizFinished] = useState(false)
+
+	// A state variable for score
+    const [score,setScore] = useState(0)
+
+	return (
+		<div className="app">
+			{quizFinished ? (
+				<div className="score-section">
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className="question-section">
+						<div className="question-count">
+							<span>Question 1</span>/{questions.length}
+						</div>
+						<div className="question-text">
+							{questions[currentIndex].questionText}
+						</div>
+					</div>
+					<div className="answer-section">
+						{questions[currentIndex].answerOptions.map((answer) => {
+							// Add onClick listener to this button
+							return (
+								<button
+									onClick={()=>handleAnswerClick(answer.isCorrect)}
+									key={answer.answerText}
+								>
+									{answer.answerText}
+								</button>
+							)
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	)
 }
-
-export default App;
